c06ee4369f5d38da59a76f01a3ead6b6e4e80cde
k1070823 <k1070823@kcl.ac.uk>
2015-12-08 15:14:46 +0000

    add icons
    add icons on the green screen

--- app/src/main/java/com/wisteria/projectwalk/controllers/MainActivity.java
+++ app/src/main/java/com/wisteria/projectwalk/controllers/MainActivity.java
@@ -1,11 +1,13 @@
 package com.wisteria.projectwalk.controllers;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.wisteria.projectwalk.R;
 import com.wisteria.projectwalk.models.Category;
@@ -27,33 +29,54 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         leaderboardContainerLayout = (LinearLayout) findViewById(R.id.leaderboard_view);
         leaderboardLayout = new LeaderboardLayout(this);
         leaderboardContainerLayout.addView(leaderboardLayout);
+
+        Button button = (Button)findViewById(R.id.button_cloud);
+        Button button2 = (Button)findViewById(R.id.button_fuel);
+        Button button3 = (Button)findViewById(R.id.button_tree);
+        Typeface font = Typeface.createFromAsset(getAssets(), "fontawesome-webfont.ttf");
+        //Set the typeface
+        button.setTypeface(font);
+        button2.setTypeface(font);
+        button3.setTypeface(font);
 
         View.OnClickListener listner = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 switch(v.getId()){
-                    case R.id.button:
+                    case R.id.button_cloud:
                         manager.setCategory(Category.C02Emissions);
                         break;
 
-                    case R.id.button2:
+                    case R.id.button_fuel:
                         manager.setCategory(Category.ForestArea);
                         break;
 
-                    case R.id.button3:
+                    case R.id.button_tree:
                         manager.setCategory(Category.FossilFuel);
                         break;
                 }
                //manager.setCategory(Category.);
             }
         };
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(listner);
+
+
+        //button.setOnClickListener(listner);
     }
+
+    public void onClick(View view) {
+        Toast.makeText(this,"C02Emissions",Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClick2(View view) {
+        Toast.makeText(this,"ForestArea",Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClick3(View view) {
+        Toast.makeText(this,"FossilFuel",Toast.LENGTH_SHORT).show();
+    }
+
+
 }
